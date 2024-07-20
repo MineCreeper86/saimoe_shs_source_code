1c49c456cb43d08b277f3cf64e6db49296ea6db2
Baka Cirno 999 <mineralcreepers@gmail.com>
2024-07-20 19:53:57 +0800

    离开设备同步

--- src/component/usersystem/LoginWindow.js
+++ src/component/usersystem/LoginWindow.js
@@ -14,7 +14,7 @@ function LoginWindow() {
             <p className="Tips">创建账号即代表您同意上海中学和风社存储您的部分个人信息。
                 <span onClick={displayMoreInfo}>{moreinfo?"":"了解更多"}</span>
             </p>
-            <p className="Tips">{moreinfo?"（包括浏览器类型，IP地址及屏幕参数等设备特征信息及选票内容等信息）":""}</p>
+            <p className="Tips">{moreinfo?"（包括学校、年级及性别等画像特征信息，浏览器类型、IP地址及屏幕参数等设备特征信息及选票内容等信息，用于检测刷票及用户群分析）":""}</p>
             <button className="SubmitButton">登入</button>
             <p>{callback.message}</p>
         </div>
--- src/page/Vote.js
+++ src/page/Vote.js
@@ -6,20 +6,24 @@ function Vote() {
     return (
         <Article>
             <h1>投票通道选择</h1>
-            <Portal startTime="1721375167000" endTime="1721375295000" href="/appvote/3/nomination">第三届上萌 - 角色提名</Portal>
-            <Portal startTime="1721375167000" endTime="1721375295000" href="/appvote/3/group">第三届上萌 - 小组赛</Portal>
-            <Portal startTime="1721375167000" endTime="1721375295000" href="/appvote/3/group">第三届上萌 - 八强赛</Portal>
+            <Portal startTime="1721375167000" endTime="1999999999999" href="/appvote/3/nomination">第三届上萌 - 角色提名</Portal>
+            <Portal startTime="1721375167000" endTime="1999999999999" href="/appvote/3/group">第三届上萌 - 小组赛</Portal>
+            <Portal startTime="1721375167000" endTime="1999999999999" href="/appvote/3/hachi">第三届上萌 - 八强赛</Portal>
+            <Portal startTime="1721375167000" endTime="1999999999999" href="/appvote/3/yon">第三届上萌 - 四强赛</Portal>
+            <Portal startTime="1721375167000" endTime="1999999999999" href="/appvote/3/semifinal">第三届上萌 - 半决赛</Portal>
+            <Portal startTime="1721375167000" endTime="1999999999999" href="/appvote/3/final">第三届上萌 - 决赛与季军赛</Portal>
         </Article>
     )
 }
 
 const Portal = (props) => {
     const alertEnded = () => {alert("该投票已结束")}
+    const skipTo = () => {window.location.href = props.href};
     if (Date.now() > props.endTime) {
         return <p className="Portal Ended" onClick={alertEnded}>{props.children}</p>
     } else if (Date.now() < props.startTime) {
         return <p className="Portal NotStarted">{props.children}</p>
-    } else return <p className="Portal Active"><a href={props.href}>{props.children}</a></p>;
+    } else return <p className="Portal Active" onClick={skipTo}><a href={props.href}>{props.children}</a></p>;
 }
 
 export default Vote;
