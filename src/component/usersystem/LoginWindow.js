a83c9865c56011030511461628ee051ddebf75a5
Baka Cirno 999 <14835957@qq.com>
2024-07-30 23:07:46 +0800

    in order to test

--- src/component/usersystem/LoginWindow.js
+++ src/component/usersystem/LoginWindow.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './LoginWindow.css'
+import axios from "axios";
 
 function LoginWindow() {
-    const [callback, setCallback] = React.useState({code:-1,message:"",redirect_signup:false});
+    const [callback, setCallback] = React.useState({code:-1,message:"",data:{}});
     const [moreinfo, setMoreInfo] = React.useState(false);
+    const [register, setRegister] = React.useState(false);
     const displayMoreInfo = () => {setMoreInfo(true);};
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get(
+                'https://api.shswafu.club/v0/user/info',
+                {
+                    headers: {
+                        "Window-Param-Height": window.outerHeight,
+                        "Window-Param-Width": window.outerWidth
+                    },
+                    withCredentials: true
+                }
+            )
+            setCallback(result.data);
+        }
+        fetchData();
+    })
     return (
         <div className="LoginWindow">
             <h3>登入账号</h3>
@@ -15,7 +33,7 @@ function LoginWindow() {
                 <span onClick={displayMoreInfo}>{moreinfo?"":"了解更多"}</span>
             </p>
             <p className="Tips">{moreinfo?"（包括学校、年级及性别等画像特征信息，浏览器类型、IP地址及屏幕参数等设备特征信息及选票内容等信息，用于检测刷票及用户群分析）":""}</p>
-            <button className="SubmitButton">登入</button>
+            <button className="SubmitButton">{register?"注册":"登入"}</button>
             <p>{callback.message}</p>
         </div>
     )
