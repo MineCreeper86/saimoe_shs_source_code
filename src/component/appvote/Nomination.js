6c36a14294f33d41c1cef409233700c9e46136ba
patchi <14835957@qq.com>
2024-08-25 13:33:41 +0800

    force, behavior modify3

--- src/component/appvote/Nomination.js
+++ src/component/appvote/Nomination.js
@@ -9,9 +9,6 @@ function Nomination() {
     const [femaleData, setFemaleData] = useState([]);
     const [loaded, setLoaded] = useState(false);
     const [started, setStarted] = useState(false);
-    const [submitCallback, setSubmitCallback] = React.useState("");
-    const [force, setForce] = React.useState(false);
-    const [autofill, setAutofill] = React.useState(true);
     const createSubject = () => {
         alert("功能开发中")
     }
@@ -20,17 +17,17 @@ function Nomination() {
         const [searchResult, setSearchResult] = React.useState([]);
         let status = true;
         const searchSubject = async () => {
-            if(document.getElementById('input-'+props.vid).value) {
+            if (document.getElementById('input-' + props.vid).value) {
                 const result = await axios.get(
                     'https://api.shswafu.club/v0/vote/nominate/search_character',
                     {
                         params: {
-                            keyword: document.getElementById('input-'+props.vid).value,
+                            keyword: document.getElementById('input-' + props.vid).value,
                         },
                         withCredentials: true
                     }
                 );
-                if(result.data.code === 0) setSearchResult(result.data.data);
+                if (result.data.code === 0) setSearchResult(result.data.data);
             } else setSearchResult([]);
         }
         const searchSubjectByComp = () => {
@@ -40,13 +37,12 @@ function Nomination() {
         const searchSubjectByInput = (event) => {
             setLastRequestTime(Date.now())
             event.target.style.backgroundColor = "white";
-            document.getElementById('hidden-'+props.vid).value = null;
-            setForce(false)
-            setTimeout((value)=>{
-                if(lastRequestTime < Date.now() - 500 && status && value === document.getElementById('input-'+props.vid).value) {
+            document.getElementById('hidden-' + props.vid).value = null;
+            setTimeout((value) => {
+                if (lastRequestTime < Date.now() - 500 && status && value === document.getElementById('input-' + props.vid).value) {
                     searchSubject();
                 }
-            },600,document.getElementById('input-'+props.vid).value)
+            }, 600, document.getElementById('input-' + props.vid).value)
         }
         const transformResult = (belong) => {
             let resultObjects = [];
@@ -55,8 +51,8 @@ function Nomination() {
                 let element = event.target;
                 if (element.children.length === 0) element = element.parentElement;
                 let fatherElement = "-" + element.id.split("-")[1] + "-" + element.id.split("-")[2];
-                const element1 = document.getElementById('input'+fatherElement);
-                const element2 = document.getElementById('hidden'+fatherElement);
+                const element1 = document.getElementById('input' + fatherElement);
+                const element2 = document.getElementById('hidden' + fatherElement);
                 element1.value = element.children[0].innerText
                 element1.style.backgroundColor = "#dcffee";
                 element2.value = element.id.split("-")[4];
@@ -66,12 +62,13 @@ function Nomination() {
                 const uniqueId = belong + "-" + id;
                 resultObjects.push(
                     <p
-                        id={uniqueId+'-'+element.id}
+                        id={uniqueId + '-' + element.id}
                         onClick={handleClick}
                         key={uniqueId}
                     >
                         <span>{element.name_cn === "" ? element.name_jp : element.name_cn}</span>
-                        {element.src === "" ? <span className="AnimeNotFound">（无相关动漫）</span> : <span className="Anime">（{element.src}）</span>}
+                        {element.src === "" ? <span className="AnimeNotFound">（无相关动漫）</span> :
+                            <span className="Anime">（{element.src}）</span>}
                     </p>
                 );
                 id++;
@@ -79,19 +76,19 @@ function Nomination() {
             return resultObjects;
         }
         useEffect(() => {
-            if (props.defaultValue !== undefined && props.defaultValue !== null && autofill) {
-                document.getElementById("input-"+props.vid).value = props.defaultValue.name;
-                document.getElementById("hidden-"+props.vid).value = props.defaultValue.id;
-                document.getElementById("hidden-"+props.vid).style.backgroundColor = "#dcffee";
+            if (props.defaultValue !== undefined && props.defaultValue !== null) {
+                document.getElementById("input-" + props.vid).value = props.defaultValue.name;
+                document.getElementById("hidden-" + props.vid).value = props.defaultValue.id;
+                document.getElementById("hidden-" + props.vid).style.backgroundColor = "#dcffee";
             }
-        },[]);
-        let fatherElementId = 'input-'+props.vid
+        }, []);
+        let fatherElementId = 'input-' + props.vid
         return <div>
-            <input type="hidden" id={'hidden-'+props.vid}/>
-            <span className="SequenceTag">提名{1+parseInt(props.vid.split("-")[1])}</span>
+            <input type="hidden" id={'hidden-' + props.vid}/>
+            <span className="SequenceTag">提名{1 + parseInt(props.vid.split("-")[1])}</span>
             <input type="text" className="NominationInput" id={fatherElementId}
                    onCompositionEnd={searchSubjectByComp}
-                   onCompositionStart={(event) => {
+                   onCompositionStart={() => {
                        status = false;
                    }}
                    onInput={searchSubjectByInput}/>
@@ -105,76 +102,17 @@ function Nomination() {
     }
     const generateChildTree = (prefix, initial) => {
         let lst = []
-        for(let i = 0; i < 10; i++) {
-            lst.push(<SubjectInput vid={prefix+"-"+i} defaultValue={initial[i]}/>)
+        for (let i = 0; i < 10; i++) {
+            lst.push(<SubjectInput vid={prefix + "-" + i} defaultValue={initial[i]}/>)
         }
         return lst
-    }
-    const submitNomination = async (force) => {
-        setAutofill(false)
-        let male_submission = []
-        let female_submission = []
-        let warning = false;
-        for(let i = 0; i < 10; i++) {
-            if(document.getElementById('hidden-male-'+i).value) {
-                male_submission.push(parseInt(document.getElementById('hidden-male-'+i).value));
-            } else if (document.getElementById('input-male-'+i).value) {
-                warning = true;
-                document.getElementById('input-male-'+i).style.backgroundColor = "yellow";
-            }
-        }
-        for(let j = 0; j < 10; j++) {
-            if(document.getElementById('hidden-fem-'+j).value) {
-                female_submission.push(parseInt(document.getElementById('hidden-fem-'+j).value));
-            } else if (document.getElementById('input-fem-'+j).value) {
-                warning = true;
-                document.getElementById('input-fem-'+j).style.backgroundColor = "yellow";
-            }
-        }
-        if(!warning) {
-            const result = force ? await axios.post(
-            'https://api.shswafu.club/v0/vote/event',null,
-            {
-                params: {
-                    channel: "nomination",
-                    event: "submit",
-                    m: encodeURIComponent(JSON.stringify(male_submission)),
-                    f: encodeURIComponent(JSON.stringify(female_submission)),
-                    force: "force"
-                },
-                withCredentials: true
-            }) : await axios.post(
-            'https://api.shswafu.club/v0/vote/event',null,
-            {
-                params: {
-                    channel: "nomination",
-                    event: "submit",
-                    m: encodeURIComponent(JSON.stringify(male_submission)),
-                    f: encodeURIComponent(JSON.stringify(female_submission)),
-                },
-                withCredentials: true
-            });
-            switch (result.data.code) {
-                case 0:
-                    setSubmitCallback("提交成功！")
-                    break;
-                case 5:
-                    setSubmitCallback(result.data.message);
-                    break;
-                case 6:
-                    setSubmitCallback(result.data.message);
-                    setForce(true);
-            }
-        } else {
-            setSubmitCallback("您提名的部分角色未知，请重新填写标黄的输入框并在弹出的搜索结果中选择一个角色或清除输入框的内容。如果没有找到要提名的角色请新建角色。")
-        }
     }
     const startNomination = async () => {
         if (!loaded) {
             console.log("正在加载数据，请稍候");
         } else {
             const result = await axios.post(
-                'https://api.shswafu.club/v0/vote/event',null,
+                'https://api.shswafu.club/v0/vote/event', null,
                 {
                     params: {
                         channel: "nomination",
@@ -189,7 +127,7 @@ function Nomination() {
     useEffect(() => {
         async function apply() {
             const result = await axios.post(
-                'https://api.shswafu.club/v0/vote/event',null,
+                'https://api.shswafu.club/v0/vote/event', null,
                 {
                     params: {
                         channel: "nomination",
@@ -197,7 +135,7 @@ function Nomination() {
                     },
                     withCredentials: true
                 });
-            if(result.data.data.details.length === 0) {
+            if (result.data.data.details.length === 0) {
                 setLoaded(true)
             } else if (result.data.data.details.length > 0) {
                 const latest = result.data.data.details[result.data.data.details.length - 1]
@@ -206,8 +144,87 @@ function Nomination() {
                 setLoaded(true)
             }
         }
-        if(!loaded) apply().then();
-    },[document.getElementById('LoginWindow')===null])
+
+        if (!loaded) apply().then();
+    }, [document.getElementById('LoginWindow') === null])
+    const NominationSubmit = () => {
+        const [submitCallback, setSubmitCallback] = React.useState("");
+        const [force, setForce] = React.useState(false);
+        const submitNomination = async (force) => {
+            let male_submission = []
+            let female_submission = []
+            let warning = false;
+            for (let i = 0; i < 10; i++) {
+                if (document.getElementById('hidden-male-' + i).value) {
+                    male_submission.push(parseInt(document.getElementById('hidden-male-' + i).value));
+                } else if (document.getElementById('input-male-' + i).value) {
+                    warning = true;
+                    document.getElementById('input-male-' + i).style.backgroundColor = "yellow";
+                }
+            }
+            for (let j = 0; j < 10; j++) {
+                if (document.getElementById('hidden-fem-' + j).value) {
+                    female_submission.push(parseInt(document.getElementById('hidden-fem-' + j).value));
+                } else if (document.getElementById('input-fem-' + j).value) {
+                    warning = true;
+                    document.getElementById('input-fem-' + j).style.backgroundColor = "yellow";
+                }
+            }
+            if (!warning) {
+                const result = force ? await axios.post(
+                    'https://api.shswafu.club/v0/vote/event', null,
+                    {
+                        params: {
+                            channel: "nomination",
+                            event: "submit",
+                            m: encodeURIComponent(JSON.stringify(male_submission)),
+                            f: encodeURIComponent(JSON.stringify(female_submission)),
+                            force: "force"
+                        },
+                        withCredentials: true
+                    }) : await axios.post(
+                    'https://api.shswafu.club/v0/vote/event', null,
+                    {
+                        params: {
+                            channel: "nomination",
+                            event: "submit",
+                            m: encodeURIComponent(JSON.stringify(male_submission)),
+                            f: encodeURIComponent(JSON.stringify(female_submission)),
+                        },
+                        withCredentials: true
+                    });
+                switch (result.data.code) {
+                    case 0:
+                        setSubmitCallback("提交成功！")
+                        break;
+                    case 6:
+                        setForce(true);
+                        setSubmitCallback(result.data.message);
+                        break;
+                    default:
+                        setSubmitCallback(result.data.message);
+                }
+            } else {
+                setSubmitCallback("您提名的部分角色未知，请重新填写标黄的输入框并在弹出的搜索结果中选择一个角色或清除输入框的内容。如果没有找到要提名的角色请新建角色。")
+            }
+        }
+        useEffect(()=>{setForce(false)}, [
+            () => {
+                let result = "";
+                Array.from(document.getElementsByClassName("NominationInput")).forEach((elem)=>{result = result + elem.value})
+                return result;
+            }
+        ]);
+        return (
+        <div className="NominationSubmit">
+            <button className="NominationButton" onClick={() => {
+                submitNomination(force)
+            }}>{force && "强制"}提交
+            </button>
+            <br/>
+            <p>{submitCallback}</p>
+        </div>)
+    }
     return (
         <Article>
             <h1>第三届上萌角色提名页面</h1>
@@ -219,13 +236,15 @@ function Nomination() {
                 Haruhi、团长</p>
             <p className="Akarin">&nbsp;&nbsp;赤座あかり&emsp;&emsp;搜索词：赤座あかり、赤座灯里、Akarin、あかざ あかり
                 、Akaza Akari、阿卡林、アッカリ〜ン</p>
-            <p onClick={createSubject}>部分角色可能因为太新或太冷门而未被收录，如果您无法在搜索下拉框中找到您想要的角色，请<b>单击此处创建新角色</b>。</p>
+            <p onClick={createSubject}>部分角色可能因为太新或太冷门而未被收录，如果您无法在搜索下拉框中找到您想要的角色，请<b>单击此处创建新角色</b>。
+            </p>
             <LoginWindow hide="1"/>
             <p style={{textAlign: "center"}}>--- 请完成上方的登录弹窗并按照要求验证邮箱（如有） ---</p>
             <div className="MainApp">
                 <div className="NominationStart">
                     {loaded || <span style={{margin: "0 auto"}}>正在加载历史提名数据</span>}
-                    {loaded && !started && <button className="NominationButton" onClick={startNomination}>开始提名</button>}
+                    {loaded && !started &&
+                        <button className="NominationButton" onClick={startNomination}>开始提名</button>}
                 </div>
                 {started && <div className="ChnlDivision ChnlFemale">
                     <h2>萌王提名</h2>
@@ -235,10 +254,7 @@ function Nomination() {
                     <h2>燃王提名</h2>
                     {generateChildTree("male", maleData)}
                 </div>}
-                {started && <div className="NominationSubmit">
-                    <button className="NominationButton" onClick={() => {submitNomination(force)}}>{force && "强制"}提交</button><br/>
-                    <p>{submitCallback}</p>
-                </div>}
+                {started && <NominationSubmit/>}
             </div>
         </Article>
     )
