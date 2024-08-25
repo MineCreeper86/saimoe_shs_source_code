fbe5181097602c2dd80ac4ebfe6f1c540f740e68
Baka Cirno 999 <14835957@qq.com>
2024-08-25 15:29:02 +0800

    不想解决也得解决了

--- src/component/appvote/Nomination.js
+++ src/component/appvote/Nomination.js
@@ -126,27 +126,29 @@ function Nomination() {
     }
 
     async function apply() {
-        const result = await axios.post(
-            'https://api.shswafu.club/v0/vote/event', null,
-            {
-                params: {
-                    channel: "nomination",
-                    event: "apply",
-                },
-                withCredentials: true
-            });
-        if (result.data.data.details.length === 0) {
-            setLoaded(true)
-        } else if (result.data.data.details.length > 0) {
-            const latest = result.data.data.details[result.data.data.details.length - 1]
-            setMaleData(latest.males)
-            setFemaleData(latest.females)
-            setLoaded(true)
+        if(!loaded) {
+            const result = await axios.post(
+                'https://api.shswafu.club/v0/vote/event', null,
+                {
+                    params: {
+                        channel: "nomination",
+                        event: "apply",
+                    },
+                    withCredentials: true
+                });
+            if (result.data.data.details.length === 0) {
+                setLoaded(true)
+            } else if (result.data.data.details.length > 0) {
+                const latest = result.data.data.details[result.data.data.details.length - 1]
+                setMaleData(latest.males)
+                setFemaleData(latest.females)
+                setLoaded(true)
+            }
         }
     }
 
-    if(!loaded) useEffect(() => {
-        setInterval(
+    useEffect(() => {
+        if(!loaded) setInterval(
             () => {apply().then();}
             , 2000
         )
