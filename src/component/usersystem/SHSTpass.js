853d377e6023ed7544a19480fa93bc5366a7cb04
Baka Cirno 999 <14835957@qq.com>
2024-09-17 13:07:14 +0800

    修改逻辑

--- src/component/usersystem/SHSTpass.js
+++ src/component/usersystem/SHSTpass.js
@@ -3,7 +3,8 @@ import Article from "../Article";
 import axios from "axios";
 
 function SHSTpass() {
-    const [userData, setUserData] = React.useState({});
+    const [callback, setCallback] = React.useState({});
+    const [message, setMessage] = React.useState("");
     const [userDataLoaded, setUserDataLoaded] = React.useState(0);
     const loadData = async () => {
         const result = await axios.get(
@@ -15,8 +16,8 @@ function SHSTpass() {
                 withCredentials: true
             }
         );
-        if (result.data.code === 0) {
-            setUserData(result.data.data);
+        setCallback(result.data);
+        if (result.data.code !== 1) {
             setUserDataLoaded(2);
         } else {
             setUserDataLoaded(1);
@@ -30,10 +31,13 @@ function SHSTpass() {
             <h1>本校学生身份验证</h1>
             <h2>您已在智慧上中授权登录。</h2>
             {userDataLoaded === 0 && <p>请稍候，正在处理您的账户信息。</p>}
-            {userDataLoaded === 1 && <p>验证失败。</p>}
+            <p>{callback.message}</p>
+            {callback.code === 3 &&
+                <input/>
+            }
             {userDataLoaded === 2 && <div>
-                <p>姓&emsp;&emsp;名：{userData.name}</p>
-                <p>统一编号：{userData.id}</p>
+                <p>姓&emsp;&emsp;名：{callback.data.name}</p>
+                <p>统一编号：{callback.data.id}</p>
             </div>}
             {userDataLoaded > 0 && <p><a href={"/"}>返回主页</a></p>}
         </Article>
