9470a13e302ca11887fbbc4b32be43a785f1d645
Baka Cirno 999 <14835957@qq.com>
2024-09-30 21:15:11 +0800

    文字直播链接

--- src/page/Timeline.js
+++ src/page/Timeline.js
@@ -5,6 +5,7 @@ function Timeline() {
     return (
         <Article>
             <h1>第三届上海市上海中学最受欢迎动漫人物赏 比赛赛程</h1>
+            <p>赛事文字直播请见<a href={"https://www.shswafu.club/2024/09/30/saimoe-2024-live/"}>此处</a></p>
         </Article>
     )
 }
