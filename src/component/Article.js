2df3526de3d0e23c13bfd97221894458532a18e9
Baka Cirno 999 <14835957@qq.com>
2024-11-13 20:39:38 +0800

    重构数据库5

--- src/component/Article.js
+++ src/component/Article.js
@@ -4,7 +4,7 @@ import './Article.css'
 function Article(props) {
     return (
         <div className="Article">
-            <h2>服务器刚刚完成数据库重构，目前处于测试阶段，出现一切问题可联系微信：mofashiqiyao，或在年级群反馈。</h2>
+            <p>服务器刚刚完成数据库重构，目前处于测试阶段，出现一切问题可联系微信：mofashiqiyao，或在年级群反馈。</p>
             {props.children}
         </div>
     )
