d441670bd5c56c86365e08047816b80327196f76
Baka Cirno 999 <14835957@qq.com>
2024-11-13 18:58:04 +0800

    小组赛打开

--- src/component/Article.js
+++ src/component/Article.js
@@ -4,6 +4,7 @@ import './Article.css'
 function Article(props) {
     return (
         <div className="Article">
+            <h1 color={"red"}>服务器正在进行数据库重构测试，预计21:00完成。请同学们不要摸鱼，认真完成晚自习再来。</h1>
             {props.children}
         </div>
     )
