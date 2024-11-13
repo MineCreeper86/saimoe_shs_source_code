9fd7126cf38e974a5cbec1422afa53f259606023
Baka Cirno 999 <14835957@qq.com>
2024-11-13 20:00:16 +0800

    重构数据库3

--- src/component/Article.js
+++ src/component/Article.js
@@ -4,7 +4,7 @@ import './Article.css'
 function Article(props) {
     return (
         <div className="Article">
-            <h1 style={{color:"red"}}>服务器正在进行数据库重构测试，预计21:00完成。请同学们不要摸鱼，认真完成晚自习再来。</h1>
+            <h2 style={{color:"red"}}>服务器正在进行数据库重构测试，预计21:00完成。请同学们不要摸鱼，认真完成晚自习再来。</h2>
             {props.children}
         </div>
     )
