36a51ce6a8a3a801dfbaac6fb97918c70602ba2d
SAZrX <107967223+GrapeSkin2191@users.noreply.github.com>
2024-11-19 21:55:47 +0800

    Update Group.js
    构式

--- src/component/appvote/Group.js
+++ src/component/appvote/Group.js
@@ -37,7 +37,6 @@ function Group() {
             setLastVal(document.getElementById('input-' + props.vid).value)
             setSearchResult(props.option.characters)
             resetFocus(props.vid)
-            forceUpdate()
         }
         const searchSubjectByInput = (event) => {
             event.target.style.backgroundColor = "white";
