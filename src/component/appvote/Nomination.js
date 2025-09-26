7fecdd9690bf9e871de72ba39b0acbf0e7a8fcb0
矿井小帕 <61276297+MineCreeper86@users.noreply.github.com>
2025-09-27 00:05:35 +0800

    更新 Nomination.js

--- src/component/appvote/Nomination.js
+++ src/component/appvote/Nomination.js
@@ -248,7 +248,7 @@ function Nomination() {
     }
     return (
         <Article>
-            <h1>第三届上萌角色提名页面</h1>
+            <h1>第四届上萌角色提名页面</h1>
             <p>您最多可在萌王赛道（女子组）及燃王赛道（男子组）各提名10个角色，请勿重复提名，请勿将女性角色填入男子组或将男性角色填入女子组，请勿填写已经成为萌王或燃王的角色。部分<b>极少或极不主要</b>出场于动漫的角色可能被组委会删除，请留意。
             </p>
             <p>提名赛的角色列表获取自 Bangumi.TV，在此为其提供的便利表达感谢。</p>
@@ -257,7 +257,7 @@ function Nomination() {
                 Haruhi、团长</p>
             <p className="Akarin">&nbsp;&nbsp;赤座あかり&emsp;&emsp;搜索词：赤座あかり、赤座灯里、Akarin、あかざ あかり
                 、Akaza Akari、阿卡林、アッカリ〜ン</p>
-            <p onClick={createSubject}>部分角色可能因为太新或太冷门而未被收录，如果您无法在搜索下拉框中找到您想要的角色，请<b>单击此处创建新角色</b>。
+            <p onClick={createSubject}>部分角色可能因为太新或太冷门而未被收录，如果您无法在搜索下拉框中找到您想要的角色，请</p><p><b>单击此处创建新角色</b>。
             </p>
             <LoginWindow hide="1"/>
             <div className="MainApp">
