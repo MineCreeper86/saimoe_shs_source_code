dc2abc7f54c2a286e3bb8f9986e403be5da1d252
Baka Cirno 999 <14835957@qq.com>
2025-09-26 21:57:30 +0800

    决赛

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,10 +3,16 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <p className="Footer">
-            ©上海中学和风社 199x - 2025&emsp;
-            <a href="https://space.bilibili.com/1807276226" target="_blank" rel="noreferrer">Bilibili</a>
-        </p>)
+        <div>
+            <p className="Footer">
+                ©上海中学和风社 199x - 2025&emsp;
+                <a href="https://space.bilibili.com/1807276226" target="_blank" rel="noreferrer">Bilibili</a>
+            </p>
+            <p className="Footer">
+                如遇使用问题请邮箱发送至 feedback@shswafu2025.club
+            </p>
+        </div>
+    )
 }
 
 export default Footer;
