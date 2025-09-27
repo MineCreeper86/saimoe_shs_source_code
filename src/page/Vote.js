e999c7f2d7c1c4e5545dd6d2f15d0534cbb04905
矿井小帕 <61276297+MineCreeper86@users.noreply.github.com>
2025-09-27 11:05:49 +0800

    Update Vote.js

--- src/page/Vote.js
+++ src/page/Vote.js
@@ -6,12 +6,12 @@ function Vote() {
     return (
         <Article>
             <h1>投票通道选择</h1>
-            <Portal startTime="1721375167000" endTime="1760228400000" href="/appvote/3/nomination">第四届上萌 - 角色提名</Portal>
-            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/3/group">第四届上萌 - 小组赛</Portal>
-            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/3/eight">第四届上萌 - 八强赛</Portal>
-            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/3/four">第四届上萌 - 四强赛</Portal>
-            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/3/semifinal">第四届上萌 - 半决赛</Portal>
-            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/3/final">第四届上萌 - 决赛与季军赛</Portal>
+            <Portal startTime="1721375167000" endTime="1760228400000" href="/appvote/4/nomination">第四届上萌 - 角色提名</Portal>
+            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/4/group">第四届上萌 - 小组赛</Portal>
+            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/4/eight">第四届上萌 - 八强赛</Portal>
+            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/4/four">第四届上萌 - 四强赛</Portal>
+            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/4/semifinal">第四届上萌 - 半决赛</Portal>
+            <Portal startTime="9999999999999" endTime="9999999999999" href="/appvote/4/final">第四届上萌 - 决赛与季军赛</Portal>
         </Article>
     )
 }
